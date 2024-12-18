28cd7c2eeb51417a318ce7015fe7681684519fb6
Adrian Estevez <adrianeea.ae@gmail.com>
2024-12-18 15:32:53 -0400

    Regenerate schemas

--- contracts/nft/marketplace-legacy/examples/schema.rs
+++ contracts/nft/marketplace-legacy/examples/schema.rs
@@ -3,7 +3,7 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use marketplace_legacy::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use marketplace_legacy::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, TokensResponse, TokenResponse, ConfigResponse};
 use marketplace_legacy::state::{Config, Token};
 
 fn main() {
@@ -14,7 +14,11 @@ fn main() {
 
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(TokensResponse), &out_dir);
+    export_schema(&schema_for!(TokenResponse), &out_dir);
+    export_schema(&schema_for!(ConfigResponse), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(Config), &out_dir);
     export_schema(&schema_for!(Token), &out_dir);
+
 }
